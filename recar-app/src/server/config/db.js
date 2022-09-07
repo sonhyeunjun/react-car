b14983b9af556b736e8fc77f102c8f1c7bce12d3
son <roh9618@nate.com>
2022-09-07 09:35:22 +0900

    출력(0)

--- recar-app/src/server/config/db.js
+++ recar-app/src/server/config/db.js
@@ -1,12 +1,9 @@
-// 모듈과 관련된 객체에 소속되어 있는 db 메소드에 객체 전달하는 것으로 약속
-var mysql = require('mysql');
+let mysql = require('mysql');
 const db = mysql.createPool({
-    host : 'localhost', // db 서버가 어떤 컴퓨터에 있는지
-    user : 'root', // 유저
+    host : 'localhost', // DB
+    user : 'root', //DB 아이디
     password : 'root', // 비번
-    database : 'used_car_db' //db이름
-}); // 이 결과가 db라는 변수에 담김
+    database : 'testdb' // DB이름
+});
 
-module.exports = db; 
-// 리턴되는 값은 항상 module.exports 이다
-
+module.exports = db;
